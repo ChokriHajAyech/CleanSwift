917d7ce76552e60d221de7632526e5ecd2d6001b
Chokri Haj Ayech <haj.ayech@gmail.com>
2021-03-04 22:44:27 +0100

    Update Conf.swift

--- Zelty/Common/Config/Conf.swift
+++ Zelty/Common/Config/Conf.swift
@@ -11,7 +11,7 @@ struct Conf {
         static let scheme = "https"
         static let host = "api.openweathermap.org"
         static let path = "/data/2.5"
-        static let key = "aacc07b2d4eb4ae5ba11e929dcf55356"
+        static let key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         static let forecast = "/forecast"
         static let weather = "/weather"
     }
